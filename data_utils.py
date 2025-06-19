29c569cd9d4cd6b8d56c6de2ae18cff2eedc58ee
xywen97 <xywen22@cse.cuhk.edu.hk>
2025-06-19 10:59:27 +0800

    update prompt

--- data_utils.py
+++ data_utils.py
@@ -3,8 +3,8 @@ from datasets import load_dataset
 SYSTEM_PROMPT = (
     "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
     "first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning "
-    "process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
-    "<think> reasoning process here </think><answer> answer here </answer>"
+    "process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, and the final answer is enclosed within \\boxed\{\} tags, i.e., "
+    "<think> think process here </think><answer> solution here \\boxed\{your final answer here\} </answer>"
 )
 
 def make_conversation(example):
